d8d5bbd8e1495a44ce4c2806a4bb7471cc141bb2
Leo <leoliu5550@gmail.com>
2023-10-24 01:16:58 +0800

    fix(Major): copy official repository
    BREAKING CHANGE: for temp meeting,but still can be official

--- labtory/draw.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-mu, sigma = 0, 0.1
-s = np.random.normal(mu, sigma, 1000)
-plt.hist(s) 
-
-plt.show()
--- labtory/torch_model_regiser.py
+++
@@ -1,8 +0,0 @@
-import torch 
-import torch.nn as nn 
-import torch.nn.functional as F 
-from src.core import register
-
-
-@register
-class HybridEncoder(nn.Module):
