84932929a64dbf495c6fc7cfaa649737f8276843
Leo <leoliu5550@gmail.com>
2023-10-28 16:36:54 +0800

    feat(authur): reset to origin
    code is not work when remove to upper floder

--- lab/regis.py
+++
@@ -1,9 +0,0 @@
-from reg import import_all_modules_for_register
-from reg import Registers
-
-
-# print("Registers.model.dict before: ", Registers.model.dict)
-# import_all_modules_for_register()
-# print("Registers.model.dict after: ", Registers.model.dict)
-
-print(Registers.model())
