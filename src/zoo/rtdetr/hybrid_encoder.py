49bfb372914a3f34f2aad46e2533a4bbff23f97d
Leo <leoliu5550@gmail.com>
2023-11-24 15:26:03 +0800

    feat(add-deformConv): add deformConv to hybird_encoder.py before last layer in feat(s5) feed into transformer
    BREAKING CHANGE:

--- src/zoo/rtdetr/hybrid_encoder.py
+++ src/zoo/rtdetr/hybrid_encoder.py
@@ -5,6 +5,7 @@ import copy
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F 
+from torchvision.ops import deform_conv2d
 
 from .utils import get_activation
 
@@ -13,7 +14,33 @@ from src.core import register
 
 __all__ = ['HybridEncoder']
 
+class DeformConvBlock(nn.Module):
+    def __init__(self,ch_in, ch_out, kernel_size, stride, padding=None, bias=False, act='leaky_relu'):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            ch_in, 
+            ch_out, 
+            kernel_size, 
+            stride, 
+            padding=(kernel_size-1)//2 if padding is None else padding, 
+            bias=bias
+            )
+        self.conv_offset = nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        # nn.init.constant_(self.conv_offset.weight,0)
+        nn.init.trunc_normal_(self.conv_offset.weight)
+        self.norm = nn.BatchNorm2d(ch_out)
+        self.act = nn.Identity() if act is None else get_activation(act) 
 
+    def forward(self, x):
+        offset = self.conv_offset(x)
+        # mask = self.sig(self.conv_mask(x)) 
+
+        out = deform_conv2d(input=x, offset=offset, 
+                                    weight=self.conv.weight, 
+                                        mask=None, padding=(1, 1))
+        
+        out = self.act(out)
+        return out
 
 class ConvNormLayer(nn.Module):
     def __init__(self, ch_in, ch_out, kernel_size, stride, padding=None, bias=False, act=None):
@@ -210,13 +237,23 @@ class HybridEncoder(nn.Module):
         
         # channel projection
         self.input_proj = nn.ModuleList()
-        for in_channel in in_channels:
-            self.input_proj.append(
-                nn.Sequential(
-                    nn.Conv2d(in_channel, hidden_dim, kernel_size=1, bias=False),
-                    nn.BatchNorm2d(hidden_dim)
+        for layer_idx,in_channel in enumerate(in_channels) :
+            # add deform convlayer
+            if layer_idx == len(in_channels)-1:
+                self.input_proj.append(
+                    nn.Sequential(
+                        # let deform conv remain same size after deformconv
+                        DeformConvBlock(in_channel, hidden_dim, kernel_size=3, stride=1, padding=1,bias=True),
+                        nn.BatchNorm2d(hidden_dim)
+                    )
                 )
-            )
+            else:
+                self.input_proj.append(
+                    nn.Sequential(
+                        nn.Conv2d(in_channel, hidden_dim, kernel_size=1, bias=False),
+                        nn.BatchNorm2d(hidden_dim)
+                    )
+                )
 
         # encoder transformer
         encoder_layer = TransformerEncoderLayer(
